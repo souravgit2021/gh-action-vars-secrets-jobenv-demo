f4d6910e6ad5b6836056d602df30440db124fe5c
Sourav Biswas <SouravBiswas@mail.com>
2026-03-23 14:51:05 +0530

    fixed_db

--- data/database.js
+++ data/database.js
@@ -5,7 +5,10 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@{clusterAddress}/?appName=Cluster0`;
+             
+
+            //  `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
