c1d8be933465e651d8e1775d2a93d9be5b637757
Sourav Biswas <SouravBiswas@mail.com>
2026-03-23 15:01:27 +0530

    bhtrhrthsrt

--- data/database.js
+++ data/database.js
@@ -5,7 +5,7 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@{clusterAddress}/?appName=Cluster0`;
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
              
 
             //  `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
